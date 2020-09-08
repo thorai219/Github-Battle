08b796a6d9495bd3c2c21b2501d10c8e9f7b975e
Terry Lee <slsk0219@gmail.com>
2020-09-08 12:21:19 -0700

    First commit for Tyler McGinnis React Course Project

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+import Popular from './components/popular'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    )
+  }
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app')
+)
