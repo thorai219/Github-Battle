bcc3889c1803d9aeb7b93209321b733434d0fa77
Terry Lee <slsk0219@gmail.com>
2020-09-17 12:02:42 -0700

    fixed navbar

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,19 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function LanguagesNav ({ selected, onUpdateLanguages }) {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+    
+  return (
+    <ul className = 'flex-center'>
+      {languages.map((language) => (
+        <li ke y= {language}>
+          <button 
+            className = 'btn-clear nav-link'
+            style = {language === selected ? { color: 'rgb(187, 46, 31)' } : null }
+            onClick = {() => onUpdateLanguages(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 export default class Popular extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage : 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render() {
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const { selectedLanguage } = this.state;
+
     return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button className='btn-clear nav-link'>
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav 
+          selected = { selectedLanguage }
+          onUpdateLanguages = { this.updateLanguage }
+        />
+      </React.Fragment>
     )
   }
 }
