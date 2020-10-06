4fc99184562d64356bedadbf538d22c07f513352
Terry Lee <slsk0219@gmail.com>
2020-10-06 12:11:25 -0700

    creating components to reduce repeating code

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -5,6 +5,7 @@ import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 import Card from './Card'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 function LanguagesNav ({ selected, onUpdateLanguages }) {
   const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -48,10 +49,12 @@ function ReposGrid ({ repos }) {
             >
               <ul className = 'card-list'>
                 <li>
-                  <FaUser color = 'rgb(255, 191, 116)' size = { 22 } />
-                  <a href = {`https://github.com/${login}`}>
-                    { login }
-                  </a>
+                  <Tooltip text="Github Username">
+                    <FaUser color = 'rgb(255, 191, 116)' size = { 22 } />
+                    <a href = {`https://github.com/${login}`}>
+                      { login }
+                    </a>
+                  </Tooltip>
                 </li>
                 <li>
                   <FaStar color = 'rgb(255, 215, 0)' size = { 22 } />
